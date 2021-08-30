0ed8e6ba11047499e3d95eb577091b56c0bfa2ce
ralexrivero <ralexrivero@gmail.com>
2021-08-29 22:51:31 -0400

    for loops

---
+++ 00-basics/02-loops_for.py
@@ -0,0 +1,14 @@
+#
+# for loop
+#
+
+def main():
+    x = 0
+
+    # for loop
+    # 10 is not inclusive in the range
+    for x in range(5, 10):
+        print(x)
+
+if __name__ == "__main__":
+    main()
