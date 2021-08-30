971d3b3ba6a5ba14ed03e5dbc2ad6fbcbe4add61
ralexrivero <ralexrivero@gmail.com>
2021-08-29 21:45:40 -0400

    conditional statement

---
+++ 00-basics/01-conditional_if.py
@@ -0,0 +1,16 @@
+#
+# conditional if
+#
+
+def main():
+    x, y = 10, 100
+
+    # conditional flow uses if, elif and else
+
+    if (x < y):
+        st = "x is less than y"
+    
+    print(st)
+
+if __name__ == "__main__":
+    main()
