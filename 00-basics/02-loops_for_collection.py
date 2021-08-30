bbdc43a22406a23e24ac603d398c6a949871a666
ralexrivero <ralexrivero@gmail.com>
2021-08-29 22:59:17 -0400

    loops for over collections

---
+++ 00-basics/02-loops_for_collection.py
@@ -0,0 +1,11 @@
+#
+# use a loop over a collection
+#
+
+def main():
+    # collection
+    days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    for d in days:
+        print (d)
+if __name__ == "__main__":
+    main()
