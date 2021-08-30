9ddf0c8938e87354c99cbd5ad3e6fbb719a9f165
ralexrivero <ralexrivero@gmail.com>
2021-08-29 23:18:52 -0400

    loop enumerate

---
+++ 00-basics/02-loop_enumerate.py
@@ -0,0 +1,10 @@
+#
+# enumerate function go get index
+#
+
+def main():
+    days = ["Mon", "Thu", "Wed", "Thu", "Fri", "Sat", "Sun"]
+    for i, d in enumerate(days):
+        print(i, d)
+if __name__ == "__main__":
+    main()
