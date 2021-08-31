6696e9294a2edc5cb450a7c17f67446c488bc3e3
ralexrivero <ralexrivero@gmail.com>
2021-08-31 14:53:58 -0400

    datetime object

--- 00-basics/05-datetime_datetime.py
+++ 00-basics/05-datetime_datetime.py
@@ -11,5 +11,8 @@ def main():
     today = datetime.now()
     print("The current date and time is: ", today)
 
+    t = datetime.time(datetime.now())
+    print("The time is: ", t)
+
 if __name__ == "__main__":
     main()
