519ef64b2abe29e7f8ac7c6de6de03378b8d7cb4
ralexrivero <ralexrivero@gmail.com>
2021-08-31 23:36:22 -0400

    time deltas

--- 00-basics/05-time_deltas.py
+++ 00-basics/05-time_deltas.py
@@ -17,8 +17,26 @@ print("Today is: " + str(now))
 # print today's day date one year from now
 print("One year from now it will be: " + str(now + timedelta(days = 365)))
 
-#printf more arguments
+# printf more arguments
 print("In 5 days and 2 weeks, it will be:" + str(now + timedelta(days = 5, weeks = 2)))
 
-#calculate 2 weeks ago
-t = datetime.now() - timedelta(weeks = 1)
+# calculate 69 weeks ago formatted as string
+t = datetime.now() - timedelta(weeks = 69)
+s = t.strftime("%A %B %d, %Y")
+print("69 weeks ago was: " + s)
+
+# how many days until my birthday
+# today
+today = date.today()
+# my birthday
+brt = date(today.year, 9, 24)
+
+# compare to see if the target day is gone for this year
+# if it has, use the replace() function to get the date for next year
+if brt < today:
+        print("My birthday already went by %d days ago" % ((today - brt).days))
+        brt = brt.replace(year = today.year + 1)
+
+# calculate the amount of time until birthday
+time_to_brt = brt - today
+print("It's just: ", time_to_brt.days, "days until my birthday")
