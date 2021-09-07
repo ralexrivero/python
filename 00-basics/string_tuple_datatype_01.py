d3bcb7e0e528f2887f55079838bc54ed3a14a5f3
ralexrivero <ralexrivero@gmail.com>
2021-09-07 12:23:52 -0400

    more tuple datatype in string formatting

---
+++ 00-basics/string_tuple_datatype_01.py
@@ -0,0 +1,5 @@
+string = "Sammy is a {}, {}, and {} {}!"
+print(string.format("happy", "smiling", "blue", "shark"))
+
+string = "Sammy is a {3}, {2}, and {1} {0}!"
+print(string.format("happy", "smiling", "blue", "shark"))
