196a7c463f157eb9c15f4b0fa635ac13bf1ec4f1
ralexrivero <ralexrivero@gmail.com>
2021-09-09 01:30:28 -0400

    modules in python

---
+++ 00-basics/06-modules_00.py
@@ -0,0 +1,30 @@
+def my_function():
+    print("In my function")
+
+my_function()
+
+def my_function():
+    print("In my function")
+
+my_function
+
+def my_function(counter):
+
+    print("Counter: {}".format(counter))
+
+my_function(12)
+
+def my_function(counter=89):
+    print("Counter: {}".format(counter))
+
+my_function(12)
+
+def my_function(counter=89):
+    print("Counter: {}".format(counter))
+
+my_function()
+
+def my_function(counter=89):
+    return counter + 1
+
+print(my_function())
