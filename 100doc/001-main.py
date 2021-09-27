a0e1de5232c92737a466ccfd0559f36482cff6ad
ralexrivero <ralexrivero@gmail.com>
2021-09-26 22:14:54 -0400

    print and quotation

--- 100doc/001-main.py
+++ 100doc/001-main.py
@@ -1,1 +1,6 @@
 print("Hello World!")
+print("print('what to print')")
+print('Using double or single quotes is the same')
+print("But you can not print the same quotation, you have to mix them")
+print('Like this: "Using double quotes"')
+# when need to print one quote use the other
