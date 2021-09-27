c3ba6eda6fba6d3dad8beda1a0b25df0e4fe5b52
ralexrivero <ralexrivero@gmail.com>
2021-09-27 11:18:26 -0400

    pep8 style fix

--- 100doc/002-string_manipulation.py
+++ 100doc/002-string_manipulation.py
@@ -1,1 +1,6 @@
+# print many lines with a single function and line of code
 print("Hello World!\nHello World!\nHello World!")
+
+# concatenate strings
+print("Hello " + "Ronald")
+print("Hello" + " " + "Ronald")
