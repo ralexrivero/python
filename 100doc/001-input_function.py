10d53eaf78fc052765069e1f749b114e4f45a22e
ralexrivero <ralexrivero@gmail.com>
2021-09-28 16:44:55 -0400

    input file

--- 100doc/001-input_function.py
+++ 100doc/001-input_function.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
 # taken data from input and replace it when print
 # get user input in console
+# when input display message, will paused until user enter the input
 print("Hello " + input("What is your name? ") + "!")
