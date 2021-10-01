3f722aead7724312986b7a284c8dacdfc951b864
ralexrivero <ralexrivero@gmail.com>
2021-10-01 17:56:34 -0300

    band name

---
+++ 100doc/001-band_name_generator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Name Generator!")
+city = input("What's name of the city you grew up in?\n")
+name = input("What's your first pet's name?\n")
+print("Your band name could be {:s} {:s}".format(city, name))
