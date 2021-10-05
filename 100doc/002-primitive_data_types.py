c13b75618c8a0893aa6c695fb9ec28b1073a0a94
ralexrivero <ralexrivero@gmail.com>
2021-10-04 21:43:57 -0300

    primitive data and typecasting

---
+++ 100doc/002-primitive_data_types.py
@@ -0,0 +1,41 @@
+# data types: string, integer, float, boolean
+
+# string
+
+print("Hello"[0])
+
+print("123" + "345")
+
+# integer
+
+print(123 + 345) 
+
+# the undersocres uses to separate decimal
+
+print(123_456_789)
+
+# float
+
+print(3.14159)
+
+#boolean
+
+True
+False
+
+num_char = len(input("What is your name?"))
+
+# typecasting int to str
+
+new_num_char = str(num_char)
+print("Your name is " + new_num_char  + " characters.")
+
+print(type(num_char))
+
+a = str(123)
+print(type(a))
+
+b = float(123)
+print(type(b))
+
+print(70 + float("100.5"))
