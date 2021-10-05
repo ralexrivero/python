21680fceba3790f2d760b1e42fbd616d726ca00a
ralexrivero <ralexrivero@gmail.com>
2021-10-05 00:04:39 -0300

    PEMDASLF

---
+++ 100doc/002-matheatical_operators.py
@@ -0,0 +1,21 @@
+print( 3 + 5)
+print( 7 - 4)
+print( 8 * 3)
+print( 10 / 2)
+print(type( 10 / 2))
+print( 16 ** 2)
+print( 16 ** 3)
+print( 16 ** 4)
+print( 16 ** 5)
+
+# PEMDASLF Left to right
+# Parentheses
+# Exponents
+# Multiplication
+# Division
+# Adition
+# Subtraction
+# () ** * / + -
+
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + (3 / 3) - 3))
