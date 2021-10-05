cfcc337c219ed38bdafdef402a63a902a6e71612
ralexrivero <ralexrivero@gmail.com>
2021-10-05 13:55:25 -0300

    simplify typecasting

--- 100doc/002-tip_calculator.py
+++ 100doc/002-tip_calculator.py
@@ -1,13 +1,10 @@
 # tip calculator
 
 print("Welcome to the tip calculator!")
-bill = input("What was the total bill? ")
-percent = input("What percentage tip would you like to give? 10, 12 or 15? ")
-people = input("How many people to split the bill? ")
+bill = float(input("What was the total bill? "))
+tip = float(input("What percentage tip would you like to give? 10, 12 or 15? "))
+people = int(input("How many people to split the bill? "))
 
-bill_f = float(bill)
-percent_f = float(percent)
-people_i = int(people)
-pay = round(bill_f * (1 + percent_f / 100) / people_i, 2)
+pay = round(bill * (1 + tip / 100) / people, 2)
 message = f"Each person should pay: $ {pay}"
 print(message)
