0f21c7a3bac75fcd5fd18a859722116f500b954f
ralexrivero <ralexrivero@gmail.com>
2021-10-07 00:57:42 -0300

    add elif in nested condition

--- 100doc/003-nested_conditional.py
+++ 100doc/003-nested_conditional.py
@@ -6,9 +6,11 @@ height = int(input("What is your height in cm? "))
 if height >= 120:
     print("You can ride the rollercoaster")
     age = int(input("What is your age? "))
-    if age >= 18:
-        print("You have to pay $12.")
-    else:
+    if age < 12:
+        print("You have to pay $5.")
+    elif age >= 12 and age < 18:
         print("You have to pay $7.")
+    else:
+        print("You have to pay 12")
 else:
     print("Sorry, you have to grow taller before you can ride.")
