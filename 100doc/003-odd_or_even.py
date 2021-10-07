df3c99b192994ac372c2ceb7bc257bd26dcc8e98
ralexrivero <ralexrivero@gmail.com>
2021-10-07 00:41:53 -0300

    odd or even

---
+++ 100doc/003-odd_or_even.py
@@ -0,0 +1,8 @@
+number = int(input("Which number do you want to check? "))
+
+if number == 0:
+    print("This is 0")
+elif number % 2 == 0:
+    print("This is an odd number")
+else:
+    print("This is an even number")
