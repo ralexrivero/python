52bc1337af7dafb2f816224a382f003cccf0885b
ralexrivero <ralexrivero@gmail.com>
2021-10-13 20:45:23 -0300

    fibonacci function

---
+++ misc/fibonacci_function.py
@@ -0,0 +1,13 @@
+def fib(n): #fibonacci series up to n
+    """print fibonacci series up to n"""
+
+
+    a, b = 0, 1
+    while a < n:
+        print(a, end=' ')
+        a, b = b, a + b
+    print()
+
+# call the function fib
+
+fib(2000)
