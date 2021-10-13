1cdf507cd5cbb87abfb3a4cdb5c6dbed6a26e014
ralexrivero <ralexrivero@gmail.com>
2021-10-13 20:34:59 -0300

    even continue

---
+++ misc/even_continue.py
@@ -0,0 +1,5 @@
+for num in range(2, 10):
+    if num % 2 == 0:
+        print(f"{num} is even")
+        continue
+    print(f"{num} is odd")
--- misc/prime.py
+++ misc/prime.py
@@ -1,4 +1,4 @@
-for n in range(0, 20):
+for n in range(50, 100):
     for x in range(2, n):
         if n % x == 0:
             print("{:d} * {:d} = {:d}".format(x, int(n / x), n))
