c3d2e6b39ee84431573ef6985674c7bf039719a1
ralexrivero <ralexrivero@gmail.com>
2021-10-16 17:47:44 -0300

    random choice

---
+++ 100doc/004-list_departaments_uruguay.py
@@ -0,0 +1,3 @@
+states_uruguay = ["Montevideo", "Salto", "Paysandu", "Maldonado", "Rivera", "Tacuarembo", "Cerro Largo", "Artigas", "Soriano", "Lavalleja", "San Jose", "Durazno", "Florida", "Treinta y Tres", "Colonia", "Rocha", "Rio Negro", "Flores", "Canelones"]
+
+print(states_uruguay)
---
+++ 100doc/004-who_pay_the_bill.py
@@ -0,0 +1,11 @@
+import random
+
+
+rand_num = random.seed(int(input("Enter a random value: ")))
+
+nameAsCSV = input("Give me everybody's names, separated by ', '\n")
+name = nameAsCSV.split(", ")
+print("{:}".format(name))
+
+banker = random.choice(name)
+print("{:s} will pay the bill".format(banker))
