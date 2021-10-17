44dcc5e1d32bb88ddced4e221327700e2d6a2c74
ralexrivero <ralexrivero@gmail.com>
2021-10-17 13:51:21 -0300

    typecasting

--- 100doc/005-high_score.py
+++ 100doc/005-high_score.py
@@ -5,8 +5,8 @@ high = 0
 
 for s in score:
     if int(s) > int(high):
-        high = s
-print(high)
+        high = int(s)
+print("The maximum score is: {:d}".format(high))
 
 """ high_max = max(score)
 print(high_max) """
