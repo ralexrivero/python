870fc10769dbc01fd01433333bdccf7743df9580
ralexrivero <ralexrivero@gmail.com>
2021-10-17 17:08:27 -0300

    password generator 3

--- 100doc/005-password3.py
+++ 100doc/005-password3.py
@@ -11,15 +11,21 @@ user_d = int(input("How many numbers would you like?\n"))
 user_s = int(input("How many symbols would you like?\n"))
 password = []
 
-for i in range(0, user_a):
-    password.append(alpha[random.randint(0, len(alpha) - 1)])
-for j in range(0, user_d):
-    password.append(digits[random.randint(0, len(digits) - 1)])
-for k in range(0, user_s):
-    password.append(special[random.randint(0, len(special) - 1)])
+m = 0
+for l in range(0, user_a + user_d + user_s):
+    if m <= user_a:
+        password.append(alpha[random.randint(0, len(alpha) - 1)])
+        m += 1
+    elif m <= user_a + user_d:
+        password.append(digits[random.randint(0, len(digits) - 1)])
+        m += 1
+    else:
+        password.append(special[random.randint(0, len(special) - 1)])
+        m += 1
 random.shuffle(password)
-for p in password:
-    print(p, end='')
+
+for n in password:
+    print(n, end='')
 print()
 
 password.clear()
