4b175e2a99fe97ab67eae1b987c5c12f37a3d431
ralexrivero <ralexrivero@gmail.com>
2021-10-17 02:22:16 -0300

    syntax fix

--- 100doc/004-treasure_chest.py
+++ 100doc/004-treasure_chest.py
@@ -6,8 +6,8 @@ row1 = [s, s, s]
 row2 = [s, s, s]
 row3 = [s, s, s]
 
-c3r3 = [row1, row2, row3]
-print("{}\n{}\n{}".format(c3r3[0], c3r3[1], c3r3[2]))
+map = [row1, row2, row3]
+print("{}\n{}\n{}".format(map[0], map[1], map[2]))
 
 position = int(input("Where do you want to put the treasure?\n\
     enter two digits: column and row. i.e. 23\n"))
@@ -15,6 +15,6 @@ position = int(input("Where do you want to put the treasure?\n\
 col = int(position / 10)
 row = position % 10
 
-c3r3[row -1][col-1] = t
+map[row -1][col-1] = t
 
-print("{}\n{}\n{}".format(c3r3[0], c3r3[1], c3r3[2]))
+print("{}\n{}\n{}".format(map[0], map[1], map[2]))
