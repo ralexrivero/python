fcd7386fe64cdbf9499fc29df7707e3e4d860cbf
ralexrivero <ralexrivero@gmail.com>
2021-11-07 20:46:15 -0300

    class person

---
+++ 07-oop/07-class_person.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Define class person
+"""
+
+class Person:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+
+    def personalData(self):
+        print("Name: {:s}\nAge: {:s}\nGender: {:s}\n".format(self.name, self.age, self.gender))
+
+Person001 = Person("Ronald", "41", "Male")
+Person001.personalData()
