49ce62ae2a380bdfd5c5dfaacbcb0f381ef431a4
ralexrivero <ralexrivero@gmail.com>
2021-11-07 20:34:30 -0300

    classes and objects

---
+++ 07-oop/07-classes_and_objects.py
@@ -0,0 +1,18 @@
+# Class and objects
+
+class Jelly:
+    # Init constructor to initilize the class
+    def __init__(self, brand, flavor, color, size):
+        self.brand = brand
+        self.flavor = flavor
+        self.color = color
+        self.size = size
+
+    def print_Jelly(self):
+        print("The Jelly's brand: {:s} is flavor: {:s}, color: {:s} and size: {:s}".format(self.brand, self.flavor, self.color, self.size))
+
+jelly1 = Jelly("Royarina", "Strawberry", "red", "medium")
+jelly2 = Jelly("Royal", "Apple", "Green", "Big")
+
+jelly1.print_Jelly()
+jelly2.print_Jelly()
