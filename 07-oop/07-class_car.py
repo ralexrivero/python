4b52ca5c604f18e0fe441458a47668f5f0fa3e62
ralexrivero <ralexrivero@gmail.com>
2021-11-08 00:05:26 -0300

    class and __str__

---
+++ 07-oop/07-class_car.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+class car
+"""
+class Car:
+    def __init__(self):
+        self.brand = "Mercedes"
+        self.year = 2021
+        self.color = "Red"
+        self.tires = 4
+        self.on = False
+    """ methods """
+    def startCar(self, start):
+        self.on = start
+        """ if on == True """
+        if(self.on):
+            return "The car is running"
+
+    def __str__(self):
+        return "Car properties\nBrand: {:s}\nYear: {:d}\nColor{:s}\nTires:{:d}\nOn:{}\n".format(self.brand, self.year, self.color, self.tires, self.on)
+
+carOne = Car()
+carOne.startCar(True)
+print(str(carOne))
