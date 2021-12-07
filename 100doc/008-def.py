29dbe5512a0bd2e0a140ec5ee6a8249dcd0ab2ae
ralexrivero <ralexrivero@gmail.com>
2021-12-07 01:03:41 -0300

    pass arguments and keyworded arguments

--- 100doc/008-def.py
+++ 100doc/008-def.py
@@ -4,13 +4,45 @@ Define and call a function to greet
 """
 
 
-def greet():
+def line_separator():
+    print("\n", "*"*80, "\n")  # separator line
+
+
+def greet(name):
     """
     display greetings
-       """
-    print("Hello!")
-    print(f"Hello everybody!")
-    print("{:s}".format("Hello again!"))
+    """
+    print("Hello " + name + " !")
+    print(f"Hello everybody and specially to {name} !")
+    print("{:s} {:s}!".format("Hello again", name))
 
 
-greet()
+"""
+name acts like parameter and value setted is the argument
+"""
+greet("Betty")
+
+
+def greet_two_param(name, location):
+    """
+    function recives two parameters
+    """
+    print("Hello {:s}".format(name))
+    print("What is it like in {:s}?".format(location))
+
+
+line_separator()
+
+greet_two_param("Ronald", "Montevideo")
+"""
+assign positional argument, order matters here
+"""
+
+line_separator()
+
+greet_two_param(location="World Trade Center", name="Jagger")
+"""
+use keyworded arguments to call the function, here doesen't matter the order
+"""
+
+line_separator()
