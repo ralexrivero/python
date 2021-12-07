2e028bd29713bd8b860bec5a1b2696693fb8c6a0
ralexrivero <ralexrivero@gmail.com>
2021-12-07 08:37:49 -0300

    delete print debugger and add border case

--- 100doc/008-prime_number_checker.py
+++ 100doc/008-prime_number_checker.py
@@ -5,8 +5,9 @@ Check if a given number is a prime number
 
 
 def prime_checker(number):
+    if number <= 1:
+        return False
     for i in range (2, number - 1):
-        print(i)
         if number % i == 0:  # not reminder, is not a prime
             return False
     return True
