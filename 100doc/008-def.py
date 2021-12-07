39ebf0934c4513ad92976c55516079c1f1f6ee0f
ralexrivero <ralexrivero@gmail.com>
2021-12-06 23:59:01 -0300

    add function declaration and apply propper style

---
+++ 100doc/008-def.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Define and call a function to greet
+"""
+
+
+def greet():
+    """
+    display greetings
+       """
+    print("Hello!")
+    print(f"Hello everybody!")
+    print("{:s}".format("Hello again!"))
+
+
+greet()
