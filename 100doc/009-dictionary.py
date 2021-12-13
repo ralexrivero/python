c2ad42980bd30d4f554a0915b979242c778c3376
ralexrivero <ralexrivero@gmail.com>
2021-12-12 22:38:30 -0300

    dictionaries

--- 100doc/009-dictionary.py
+++ 100doc/009-dictionary.py
@@ -5,11 +5,28 @@ Dictionaries
 """
 
 
-languages = {
+technologies = {
     "c": "Low level programming",
     "Python": "Higer level programming",
     "JavaScript": "Web development language",
     "ReactJs": "JavaScript framework",
     "Java": "Higer level programming",
-    "SQL": "Database language"
+    "SQL": "Database language",
+    "TensorFlow": "Machine Learning platform",
+    "html": "Markup language for web structure",
 }
+# retrieve values from dictionary
+print(technologies["ReactJs"])
+
+# adding new values to a dictionary
+technologies["CSS"] = "Style language for web design"
+
+print(technologies)
+
+# loop through a dictionary, retrieve keys
+for key in technologies:
+    print(key)
+
+t = technologies
+for key in t:
+    print("{:s} = {:s}".format(key, t[key]))
---
+++ 100doc/009-student_grades.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+scores = {
+    "John": 75,
+    "Ronald": 99,
+    "Clarck": 78,
+    "Mark": 69,
+    "Newton": 82,
+}
+
+grades = {}
+
+for name in scores:
+    score = scores[name]
+    if score > 90:
+        grades[name] = "Outstanding"
+    elif score > 80:
+        grades[name]= "Exceeds Expectations"
+    elif score > 70:
+        grades[name]= "Acceptable"
+    else:
+        grades[name] = "Fail"
+
+for key in grades:
+    print("{:s}: {:s}".format(key, grades[key]))
