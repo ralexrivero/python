c04742adaca729a99bf37b5a166f546a35581057
ralexrivero <ralexrivero@gmail.com>
2021-12-13 01:29:58 -0300

    add entry function

--- 100doc/009-nesting_dict.py
+++ 100doc/009-nesting_dict.py
@@ -9,10 +9,25 @@ carrer_log = [
         "Projects": 2
     },
     {
-        "Fundations_2": "Higer level programming",
+        "Fundations": "Higer level programming",
         "Languages": ["Python", "Ruby"],
         "Projects": 4
     },
 ]
+
+print(carrer_log)
+
+print("\n", "*" * 80, "\n")
+
+
+def add_new_level(fundations, languages, projects):
+    new_dict = {}
+    new_dict["Fundations"] = fundations
+    new_dict["Languages"] = [languages]
+    new_dict["Projects"] = projects
+    carrer_log.append(new_dict)
+
+
+add_new_level("Web services", ["SQL", "NGINX", "PHP", "JavaScript"], 5)
 
 print(carrer_log)
