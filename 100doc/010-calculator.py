6fe69c42fb3d491b97f0c4759f2fdfd04386ea42
ralexrivero <ralexrivero@gmail.com>
2021-12-16 16:08:57 -0300

    fix minnor bugs

--- 100doc/010-calculator.py
+++ 100doc/010-calculator.py
@@ -44,32 +44,36 @@ modes = {
         '/': div
     }
 
-print("Welcome to the Basic calculator")
-n1 = float(input("Enter a number: "))
-for f in modes:
-    print(f)
-option = input("Choose an operation: ")
-n2 = float(input("Enter the second number: "))
-first_calc = modes[option](n1, n2)
-print("{:.1f} {:s} {:.1f} = {:.1f}".format(n1, option, n2, first_calc))
-
 
 def repeat_calc():
     """
     Repeat calculation with the previous result
     """
+    print("Welcome to the Basic calculator")
+    n1 = float(input("Enter a number: "))
+    for f in modes:
+        print(f)
+    option = input("Choose an operation: ")
+    n2 = float(input("Enter the second number: "))
+    first_calc = modes[option](n1, n2)
+    print("{:.1f} {:s} {:.1f} = {:.1f}".format(n1, option, n2, first_calc))
     prev_calc = first_calc
     repeat = input("want to continue? type 'y' or 'n': ")
     while (repeat == "y"):
         for f in modes:
             print(f)
-        option = input("Pick another operation: ")
+        option = input("Pick another operation with {}: ".format(prev_calc))
         n3 = float(input("Enter the next number: "))
         next_calc = modes[option](prev_calc, n3)
         print("{:.1f} {:s} {:.1f} = {:.1f}".format(prev_calc,
               option, n3, next_calc))
-        repeat = input("want to continue? type 'y' or 'n': ")
-        prev_calc = next_calc
+        if input("want to continue? type 'y' or start new \
+operation 'n': ") == "y":
+            prev_calc = next_calc
+            repeat = "y"
+        else:
+            repeat = "n"
+            repeat_calc()
 
 
 repeat_calc()
