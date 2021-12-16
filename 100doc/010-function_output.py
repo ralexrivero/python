c20528babcc01a115aa948ec5e0caabd34a80bb9
ralexrivero <ralexrivero@gmail.com>
2021-12-16 02:01:58 -0300

    function output

--- 100doc/010-function_output.py
+++ 100doc/010-function_output.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
 Title case transform
+exist a python str.title() that can do this job
 """
 
 f_name = input("First name: ")
@@ -9,13 +10,15 @@ l_name = input("Last name: ")
 
 def title_case(f_name, l_name):
     """
-    Transform text to cammel case
+    Transform text to title case
     """
     f_name = f_name.lower()
     l_name = l_name.lower()
     f_name_T = ("{:s}{:s}".format(f_name[0].upper(), f_name[1:]))
     l_name_T = ("{:s}{:s}".format(l_name[0].upper(), l_name[1:]))
-    print("{:s} {:s}".format(f_name_T, l_name_T))
+    return f_name_T, l_name_T
+"""     print("{:s} {:s}".format(f_name_T, l_name_T)) """
 
 
-title_case(f_name, l_name)
+formatted = title_case(f_name, l_name)
+print(formatted)
