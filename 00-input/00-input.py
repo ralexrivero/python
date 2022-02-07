289a623851271700220a5e5a57211a5ae7567fbd
ralexrivero <ralexrivero@gmail.com>
2022-02-07 18:37:32 -0300

    input and ternary operator

---
+++ 00-input/00-input.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""
+captura the name and greet the user
+"""
+
+name = input('What is your name? ')
+
+print('Hello, ' + name + '!')
---
+++ 00-operators/00-ternary_operator.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Ternary operator
+"""
+
+# evaluates a condition
+
+# condition_if_true if condition else condition_if_false
+
+is_friend = True
+can_message = "message allowed" if is_friend else "not allowed"
+
+print(can_message)
