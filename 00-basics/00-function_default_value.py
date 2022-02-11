384a5ee69682fc69ab3700c38f156dad9476e8c5
ralexrivero <ralexrivero@gmail.com>
2022-02-11 14:50:27 -0300

    style and pycharm test

--- 00-basics/00-function_default_value.py
+++ 00-basics/00-function_default_value.py
@@ -1,6 +1,7 @@
-#
-# function with default value
-#
+"""
+function with default value
+"""
+
 
 def power(num, x=1):
     result = 1
@@ -8,9 +9,12 @@ def power(num, x=1):
         result = result * num
     return result
 
-# if dont pass x, the default will be 1
+
+"""
+if don't pass x, the default will be 1
+"""
 print(power(2))
-print(power(2,3))
-print(power(3,4))
-# supply the name of the values. Dont need to pass in a particular order if use names 
-print(power(x = 4, num = 3))
+print(power(2, 3))
+print(power(3, 4))
+"""supply the name of the values. Don't need to pass in a particular order when use names """
+print(power(x=4, num=3))
