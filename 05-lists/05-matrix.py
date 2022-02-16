452e9a7e9ea08e3ca32acb8307e29cfbca30f747
ralexrivero <ralexrivero@gmail.com>
2022-02-16 08:54:11 -0300

    matrix list

---
+++ 05-lists/05-matrix.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# matrix list
+
+""" multi dimensional list """
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+""" access the list """
+
+print(matrix)
+print(matrix[0])
+print(matrix[0][0])
+print(matrix[::-1])
+print(matrix[0:2])
