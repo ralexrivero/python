330186aa697a5d096fdd2dcf68effb7ff908a2f9
ralexrivero <ralexrivero@gmail.com>
2022-02-18 20:59:57 -0300

    dictionaries

--- 01-dictionaries/00-dict.py
+++ 01-dictionaries/00-dict.py
@@ -1,3 +1,38 @@
 #!/usr/bin/env python3
-# using dictionaries
+""" using dictionaries """
 
+d = {
+    'a': 1,
+    'b': 2,
+    'c': 3,
+    'd': 27,
+    'x': "hello",
+    'y': [1, 2, 3, 4, 5, 6],
+    'z': True,
+    'm': 5.7,
+    'role': 'Admin'
+}
+
+print(d)
+print(d['c'])
+print(d['role'])
+
+l = [
+    {
+        'a': [1, 2, 3],
+        'b': 'Hi!',
+        'c': True,
+        'D': None
+    },
+    {
+        'a': [4, 5, 6],
+        'b': 'Hi!',
+        'c': True,
+        'D': None
+    }
+]
+
+print(l)
+print(l[0])
+print(l[0]['a'][1])
+print(l[1]['a'][1])
