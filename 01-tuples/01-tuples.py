bd4d3d2fda8228c3e74d9b34a36cc788561e238d
ralexrivero <ralexrivero@gmail.com>
2022-02-19 12:37:00 -0300

    tuples and tuples methods

--- 01-tuples/01-tuples.py
+++ 01-tuples/01-tuples.py
@@ -1,7 +1,16 @@
 #!/usr/bin/env python3
 """ tuples are immutable """
 
-tp = (1, 2, 3)
+tp = (1, 2, 3, 5, 4, 5, 8, 9, 1)
 
 print(tp[1])
 print(3 in tp)
+
+x, y, z, *other = tp
+
+print("x: {}\ny: {}\nz: {}\nother: {}".format(x, y, z, other))
+
+print("tp length is: {}".format(len(tp)))
+
+print(tp.count(5))
+print(tp.index(3))
