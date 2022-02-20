a287d47353acef7a2bdd0d6bbe029cb3f437486a
ralexrivero <ralexrivero@gmail.com>
2022-02-20 01:34:15 -0300

    conditional

---
+++ 02-conditional/02-conditional.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" conditional """
+
+is_old = True
+is_licensed = True
+
+if is_old and is_licensed:
+    print("You can drive!")
+else:
+    print("Your are not able to drive")
