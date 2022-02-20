a833c3255c7c9774952f98330378766d613635e6
ralexrivero <ralexrivero@gmail.com>
2022-02-20 13:47:36 -0300

    while

---
+++ 03-loops/03-while.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" while loop """
+
+i = 0
+while i < 5:
+    print(i)
+    i += 1
+else:
+    """
+    else statement will be only executed when while is false
+    never if a break exist
+    """
+    print('Done with all the work')
