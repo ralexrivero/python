f52b6255ac65a0f2cfedfcf121b8fad8a68dd383
ralexrivero <ralexrivero@gmail.com>
2022-02-21 16:42:56 -0300

    print pixels

---
+++ misc/pixel_emulate.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# emulates pixel output
+
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
+]
+
+for line in picture:
+    for x in line:
+        if x == 0:
+            print(' ', end='')
+        else:
+            print('*', end='')
+    print()
