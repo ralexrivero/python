8dfa501ff10fdcf755f809affeb27977de418663
ralexrivero <ralexrivero@gmail.com>
2022-02-21 18:22:39 -0300

    function parameters and arguments

--- 04-functions/04-parameters_vs_arguments.py
+++ 04-functions/04-parameters_vs_arguments.py
@@ -2,20 +2,29 @@
 """
     functions allows to don't DRY
     DRY: 'don't repeat yourself'
+    Positional parameters and arguments mean that the position matters
 """
 
 
-def greet(name='Person'):
-    """ greet with parameters """
-    print('Hello {}!'.format(name))
+def greet(treatment='Mr', name='Person'):
+    """
+    greet with positional parameters
+    and default parameters
+    """
+    print('Hello {} {}!'.format(treatment, name))
 
 
 greet()
 
 """ call the function providing arguments """
-greet('Ronald')
+greet('FSE', 'Ronald')
 
-greet(name='Alexander')
+"""
+    keyword arguments
+    Is a good practice to follow the flow of the program
+    Should use the same order
+"""
+greet(treatment='Sr', name='Alexander')
 
 print('The function \'greet()\' is allocated in: {}\ntype: {}'
       .format(greet, type(greet)))
---
+++ 04-functions/04-return.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+    function should:
+        must do one thing well
+        return a value
+"""
+
+from pyrsistent import b
+
+
+def sum(a, b):
+    """ add two numbers, return the result """
+    return a + b
+    """ return exits the function """
+
+
+print(sum(5, 4))
