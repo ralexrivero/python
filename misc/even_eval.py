a5cb4e700ad126fa292d7586dd3fd709a0c95cae
ralexrivero <ralexrivero@gmail.com>
2022-02-21 23:31:41 -0300

    evaluate even number like a boolean expression

---
+++ misc/even_eval.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" checks if number is even or odd like a boolean expression """
+
+
+def is_even(num):
+    """Check if number is even or odd. Return True or False """
+    return num % 2 == 0
+
+
+print(is_even(2))
+
+print(is_even(123))
