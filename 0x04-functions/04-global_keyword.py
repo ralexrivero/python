f1959a35675a5182898fc21544c51c90842b2a0d
ralexrivero <ralexrivero@gmail.com>
2022-02-24 08:37:09 -0300

    rename folders

--- 0x04-functions/04-global_keyword.py
+++ 0x04-functions/04-global_keyword.py
@@ -14,3 +14,15 @@ def count():
 print(count())
 print(count())
 print(count())
+
+total2 = 0
+
+
+def count2(total2):
+    """ increase the value once """
+    total2 += 1
+    return total2
+
+
+print(count2(total2))
+print(count2(count2(count2(total2))))
