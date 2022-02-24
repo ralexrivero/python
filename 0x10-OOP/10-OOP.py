4d27da2f62c920d44667609fce3f88f80f3507d7
ralexrivero <ralexrivero@gmail.com>
2022-02-24 09:28:20 -0300

    type of built in classes

--- 0x10-OOP/10-OOP.py
+++ 0x10-OOP/10-OOP.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3
-""" 
+""" object oriented programming """
+
+from socket import J1939_EE_INFO_NONE
+
+
+a = None
+b = True
+c = 2
+d = 1.1
+e = 'hello'
+f = []
+g = {1, }
+h = {}
+i = ()
+j = complex(5, 7)
+
+types = [a, b, c, d, e, f, g, h, i, j]
+
+for x in types:
+    print('{}'.format(type(x)))
