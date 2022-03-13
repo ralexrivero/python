af0a602b6603e837fcca5a203bc690dd2b832229
ralexrivero <ralexrivero@gmail.com>
2022-03-13 13:42:54 -0300

    template with jinja

--- 100_days_of_code/54-flask/app.py
+++ 100_days_of_code/54-flask/app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
+from datetime import datetime
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return '<p>Hello World!</p>'
+    random_number = random.randint(1, 10)
+    currYear = datetime.now().year
+    return render_template('index.html', num=random_number, year=currYear)
 
 
 if __name__ == '__main__':
