6010b4b5d8e540de5f640b20036d4264d376bf37
ralexrivero <ralexrivero@gmail.com>
2022-05-29 02:25:46 -0300

    add attributes to PlayerCharacter

--- ZTM-CPD2022/6-OOP/0x06-objects.py
+++ ZTM-CPD2022/6-OOP/0x06-objects.py
@@ -4,17 +4,24 @@
 
 class PlayerCharacter:
     """ class PlayerCharacter """
-    def __init__(self, name):
+    def __init__(self, name, age):
         # atuomatically called when instantiate the class
         self.name = name
+        self.age = age
         # self refers to the object that will be created (playerOne)
 
     def run(self):
         print('run')
 
 
-playerOne = PlayerCharacter('Harry')
-playerTwo = PlayerCharacter('')
+playerOne = PlayerCharacter('Harry', 43)
+playerTwo = PlayerCharacter('Ronald', 41)
+
+print('PlayerOne')
 print(playerOne)
-print(playerOne.name)
+print(playerOne.name, playerOne.age)
 playerOne.run()
+
+print('PlayerTwo')
+print(playerTwo.run())
+playerTwo.run()
