0be3e1af8a6d74f944c0c42b2a2a2c494b263406
ralexrivero <ralexrivero@gmail.com>
2022-05-31 19:43:06 -0300

    style fix

--- ZTM-CPD2022/6-OOP/0x06-staticmethod.py
+++ ZTM-CPD2022/6-OOP/0x06-staticmethod.py
@@ -3,6 +3,7 @@
 
 class PlayerCharacter:
     membership = True
+
     def __init__(self, name, age):
         self.name = name
         self.age = age
@@ -10,11 +11,18 @@ class PlayerCharacter:
     def shout(self):
         print("Hello i am {}".format(self.name))
 
+    def run(self):
+        return self
+
     @classmethod
     def adding_things(cls, num1, num2):
         return cls('Teddy', num1 + num2)
 
+
 player2 = PlayerCharacter('Tom', 20)
 player3 = PlayerCharacter.adding_things(2, 3)
-print(player2.adding_things)
-print(player3)
+
+print(player2.run())
+player2.shout()
+
+player3.shout()
