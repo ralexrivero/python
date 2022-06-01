86720d176429912986725580ae92f5a32807c60a
ralexrivero <ralexrivero@gmail.com>
2022-05-31 21:10:43 -0300

    extend class

---
+++ ZTM-CPD2022/6-OOP/0x06-SuperList.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+""" extend list """
+
+
+class SuperList(list):
+    def __len__(self):
+        return 1000
+
+super_list1 = SuperList()
+super_list1.append(5)
+print(super_list1[0])
+print(len(super_list1))  # extend the functionality of list
+print(issubclass(list, object))
+print(issubclass(SuperList, list))
