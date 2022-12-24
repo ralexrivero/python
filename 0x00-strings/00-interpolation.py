da48aabacacbb440f9a89ed62d182c22bb688988
ralexrivero <ralexrivero@gmail.com>
2022-12-24 18:29:32 -0300

    string interpolation

---
+++ 0x00-strings/00-interpolation.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+name = "John"
+age = 30
+message = "Hello, my name is %s and I am %d years old." % (name, age)
+print(message)  # Output: "Hello, my name is John and I am 30 years old."
